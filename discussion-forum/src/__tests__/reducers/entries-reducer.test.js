34ca7733e66cddf978326f977ccf3b09d865b164
Thomas Russell <t.p.russell19@gmail.com>
2021-07-07 11:33:54 -0700

    add third test for entries-reducer
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>

--- discussion-forum/src/__tests__/reducers/entries-reducer.test.js
+++ discussion-forum/src/__tests__/reducers/entries-reducer.test.js
@@ -2,12 +2,29 @@ import entriesReducer from "./../../reducers/entries-reducer.js";
 import { entries as defaultEntriesState } from './../../defaultEntries.json';
 
 describe('entriesReducer', () => {
-  test('should return empty state when passed empty state', () => {
+  let action;
+  const entry = {
+    title: "test1",
+    board: "test1",
+    body: "test1",
+    id: "test1",
+    votes: 5,
+    comments: []
+  }
+
+  test('should return empty state when pass ed empty state', () => {
     expect(entriesReducer({}, {})).toEqual({});
   });
 
   test('should return default state when passed default', () => {
     expect(entriesReducer(defaultEntriesState, {})).toEqual(defaultEntriesState);
   });
+
+  test('should successfully add entry', () => {
+    action = {
+      type: "ADD_ENTRY", ...entry
+    };
+    expect(entriesReducer(defaultEntriesState, action)).toEqual({...defaultEntriesState, ...entry})
+  })
 })
 
--- discussion-forum/src/reducers/entries-reducer.js
+++ discussion-forum/src/reducers/entries-reducer.js
@@ -1,7 +1,20 @@
 import { entries as defaultEntriesState } from './../defaultEntries.json';
 
 const entriesReducer = (state = defaultEntriesState, action) => {
-  return state;
-}
+  const { title, board, body, votes, comments, id } = action;
+  switch (action.type) {
+    case 'ADD_ENTRY':
+      return Object.assign({}, state, {
+        title: title,
+        board: board,
+        body: body,
+        votes: votes,
+        comments: comments,
+        id: id
+      });
+    default:
+      return state;
+  }
+};
 
 export default entriesReducer;
