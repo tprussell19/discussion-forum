e9a53df2defe966bdb77c25b8197e89948858ec4
Thomas Russell <t.p.russell19@gmail.com>
2021-07-07 10:06:05 -0700

    add initial files
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>

---
+++ discussion-forum/src/App.js
@@ -0,0 +1,35 @@
+
+import React, { Component } from 'react'
+import Home from './pages/Home'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentPage: this.getCurrentLocation()
+    }
+  }
+
+  getCurrentLocation = () => {
+    return window.location.pathname.slice(1)
+  }
+
+  navigateTo = (event, location) => {
+    event.preventDefault()
+    this.setState({currentPage: location})
+    window.history.replaceState({}, 'Squash Shoes', location)
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        { this.state.currentPage === 'home' ? <Home /> : null }
+        <Footer />
+      </div>
+    )
+  }
+}
+export default App
---
+++ discussion-forum/src/pages/Home.js
@@ -0,0 +1,13 @@
+
+import Header from './../components/Header'
+
+function Home() {
+  return(
+    <div style={{ minHeight: '100vh' }}>
+    <Header title='Home' />
+      // main content goes here
+    </div>
+  )
+}
+
+export default Home
